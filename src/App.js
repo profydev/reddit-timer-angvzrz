7739db5775c200539d36b794247d0c191f92b566
angvzrz <ang_vzrz@live.com.mx>
2021-09-22 00:58:06 -0700

    Created elements template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,62 @@ import React from 'react';
 function App() {
   return (
     <div>
-      App Placeholder
+      <div id="header">
+        <div id="corner-logo-container">
+          <img
+            id="corner-logo"
+            src="https://toppng.com/uploads/preview/
+                  reddit-icon-reddit-logo-transparent-115628752708pqmsy4kgm.png"
+            alt="take me home"
+            width="150"
+          />
+        </div>
+        <div id="navigation">
+          <button type="button">Search</button>
+          <button type="button">How it works</button>
+          <button type="button">About</button>
+        </div>
+      </div>
+      <div id="content">
+        <p>No reactions to your reddit posts?</p>
+        <p>
+          Great timing, great results! Find the best time to post onn your
+          subrredit.
+        </p>
+        <button type="button">SHOW ME THE BEST TIME</button>
+        <p>r/javascript</p>
+        <div>Here it goes the heatmap preview</div>
+        <div id="how-it-works">
+          <h2>How it works</h2>
+          <ul>
+            <li>We find the 500 top posts from the past year for a subrredit</li>
+            <li>The data is visualized in a heatmap grouped by weekday and hour of the day</li>
+            <li>See immediately when to submit your reddit post</li>
+          </ul>
+        </div>
+        <div id="about">
+          <h2>About</h2>
+          <p>
+            This app was created during a course on
+            {' '}
+            <a href="https://profy.dev/">profy.dev</a>
+            {' '}
+            with the goal to implement a pixel-perfect real-world application with professional
+            workflows and tools like Kanban, Asana, Zeplin, GitHub, pull requests
+            and code reviews.
+            {' '}
+            <a href="https://profy.dev/project/reddit-timer/welcome">
+              Click here for more information
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+      <div id="bottom">
+        <a href="https://profy.dev/">profy.dev</a>
+        <div>search icon here</div>
+        <a href="https://profy.dev/legal">Terms & Privacy</a>
+      </div>
     </div>
   );
 }
