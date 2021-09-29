0d24ccc67506226de3d039aaf97c6a4df2cb5186
angvzrz <ang_vzrz@live.com.mx>
2021-09-28 22:41:03 -0700

    Created theme file with default styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   return (
