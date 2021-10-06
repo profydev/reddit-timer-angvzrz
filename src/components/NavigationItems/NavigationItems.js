c136401ca2421167a3280142eaf538e7b2e45d5f
angvzrz <ang_vzrz@live.com.mx>
2021-10-06 01:25:18 -0700

    Moved components from components folder to their corresponding folder within the same components folder

--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavigationItem from './NavigationItem';
+import NavigationItem from '../NavigationItem/NavigationItem';
 
 function NavigationItems() {
   return (
