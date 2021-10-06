1e08e74256c69fe8cafa1ad20e56831394a9df55
angvzrz <ang_vzrz@live.com.mx>
2021-10-06 01:22:06 -0700

    Created and initiaded NavigationItems component

---
+++ src/components/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavigationItem from './NavigationItem';
+
+function NavigationItems() {
+  return (
+    <div>
+      <NavigationItem>Search</NavigationItem>
+      <NavigationItem>How it works</NavigationItem>
+      <NavigationItem>About</NavigationItem>
+    </div>
+  );
+}
+
+export default NavigationItems;
