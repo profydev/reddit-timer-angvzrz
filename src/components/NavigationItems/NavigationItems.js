adc362bf4c654fc21a2d1540eb455494ae22e309
angvzrz <ang_vzrz@live.com.mx>
2021-10-28 00:28:04 -0700

    Fixed routes in Links

--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -5,9 +5,9 @@ import Styling from './NavigationItemsStyles';
 function NavigationItems() {
   return (
     <Styling>
-      <NavigationItem goToPage="/search" innerHtml="Search" />
-      <NavigationItem goToPage="/how_it_works" innerHtml="How it works" />
-      <NavigationItem goToPage="/about" innerHtml="About" />
+      <NavigationItem goToPage="/search/javascript" innerHtml="Search" />
+      <NavigationItem goToPage="/#how-it-works" innerHtml="How it works" />
+      <NavigationItem goToPage="/#about" innerHtml="About" />
     </Styling>
   );
 }
