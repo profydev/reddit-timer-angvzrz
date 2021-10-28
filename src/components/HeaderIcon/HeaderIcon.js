227b067d144a0995ead76665225e757d9105c865
angvzrz <ang_vzrz@live.com.mx>
2021-10-28 00:49:31 -0700

    Changed header icon to use img tag

--- src/components/HeaderIcon/HeaderIcon.js
+++ src/components/HeaderIcon/HeaderIcon.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { ReactComponent as HeaderLogo } from '../../assets/header_logo.svg';
+import headerLogo from '../../assets/header_logo.svg';
 import Styling from './HeaderIconStyles';
 
 function HeaderIcon() {
   return (
     <Styling>
       <Link to="/">
-        <svg xmlns="http://www.w3.org/2000/svg">
-          <HeaderLogo />
-        </svg>
+        <img src={headerLogo} alt="Reddit header logo" />
       </Link>
     </Styling>
   );
